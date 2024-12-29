18d750083f81313e856eb25b18f0bab33221e94b
DarkSideAlip <01tanwaalip@gmail.com>
2024-12-29 22:48:09 +0700

    change

--- login/db_connect.php
+++ login/db_connect.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "alip4523pop";
+$password = "12345678";
 $dbname = "booking-main";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
